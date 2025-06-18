5b3d92afcfa97ecb835f6ce26bba1d7255fb3b2e
KÉPLER <tandat6444@gmail.com>
2025-06-18 12:40:48 +0700

    3

--- js/NhanVien.js
+++ js/NhanVien.js
@@ -23,11 +23,30 @@ export class NhanVien {
         } else if (soGioLam >= 160) {
             xepLoai = 'Nhân viên khá';
         } else {
-            xepLoai = 'Nhân viên trung bình'
+            xepLoai = 'Nhân viên trung bình';
         }
+        return xepLoai;
     }
 }
 
 export class QuanLyNV {
     arrNhanVien = [];
+
+    hienThiNhanVien() {
+        let bodyTable = document.querySelector(`#tableDanhSach`);
+
+        for (let indexNV in this.arrNhanVien) {
+            bodyTable.innerHTML = `
+                <tr>
+                    <td>${this.arrNhanVien[indexNV].user}</td>
+                    <td>${this.arrNhanVien[indexNV].fullName}</td>
+                    <td>${this.arrNhanVien[indexNV].email}</td>
+                    <td>${this.arrNhanVien[indexNV].date}</td>
+                    <td>${this.arrNhanVien[indexNV].position}</td>
+                    <td>${this.arrNhanVien[indexNV].tongLuong}</td>
+                    <td>${this.arrNhanVien[indexNV].xepLoai}</td>
+                    <td><em class="fa fa-cog"></td>
+            `
+        }
+    }
 }
--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ import { NhanVien, QuanLyNV } from "/js/NhanVien.js";
 document.querySelector('#btnThemNV').onclick = function () {
     let nhanVien = new NhanVien();
     console.log(nhanVien);
-    
+    let arrInput = document.querySelectorAll('.fill-input');
+    for (let tag of arrInput) {
+        let id = tag.id;
+        let value = tag.value;
+        nhanVien[id] = value;
+    }
 
 }
