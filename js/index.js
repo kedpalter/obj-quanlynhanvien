beeae7ad1826225e284fcfb12d5e9270b71a66d6
Kepler <tandat6444@gmail.com>
2025-06-21 14:19:00 +0700

    8

--- js/index.js
+++ js/index.js
@@ -19,18 +19,37 @@ window.editNhanVien = function (index) {
     document.querySelector('#btnXoa').setAttribute('data-index-edit', index);
 }
 window.xoaNhanVienUI = function (indexDel) {
-    console.log(indexDel);
+    // console.log(indexDel);
     danhSachNV.xoaNhanVien(indexDel);
     danhSachNV.hienThiNhanVien();
     // Save local
     danhSachNV.saveLocal();
 
 }
+    // ----------------------------- VALIDATION -----------------------------------
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
 
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var frmValid = document.querySelectorAll('.needs-validation');
+
+        // Loop over them and prevent submission
+        frmValid.forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    });
 
 document.querySelector('#btnCapNhat').onclick = function (e) {
     let indexEdit = e.target.getAttribute('data-index-edit');
-    console.log(indexEdit);
+    // console.log(indexEdit);
     // Bản chất cập nhật món ăn giống như thêm 1 món mới trên index cũ hiện tại
     let updateNV = new NhanVien()
     let updateArrInput = document.querySelectorAll('.fill-input');
@@ -51,15 +70,15 @@ document.querySelector('#btnCapNhat').onclick = function (e) {
 
 document.querySelector('#btnXoa').onclick = function (e) {
     let indexEdit = e.target.getAttribute('data-index-edit');
-    console.log(indexEdit);
+    // console.log(indexEdit);
     danhSachNV.xoaNhanVien(indexEdit);
     danhSachNV.hienThiNhanVien();
     danhSachNV.saveLocal();
 }
 
-document.querySelector('#btnThemNV').onclick = function (e) {
+document.querySelector('#btnThemNV').onsubmit = function (e) {
+    console.log('Nhấn nút Thêm người dùng');
     e.preventDefault();
-    document.querySelector('#btnXoa').classList.add('d-none');
 
     let nhanVien = new NhanVien();
     let arrInput = document.querySelectorAll('.fill-input');
@@ -78,4 +97,6 @@ document.querySelector('#btnThemNV').onclick = function (e) {
     danhSachNV.saveLocal();
 
 }
+
+
 
